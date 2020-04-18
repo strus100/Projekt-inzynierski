eb84f5e23bc1d2f230c197dc800e7b1d42fa1ba3
Ladavi <dawid.poz@wp.pl>
2020-04-18 17:32:13 +0200

    Menu, Chat, Footer components

--- frontendinz/src/App.js
+++ frontendinz/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Menu from "./Menu"
+import Chat from "./Chat"
+import Footer from "./Footer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChangeMenu = this.handleChangeMenu.bind(this);
+    this.handleChangeChat = this.handleChangeChat.bind(this);
+    this.state = { checkedMenu: false, checkedChat: false };
+  }
+
+  handleChangeMenu() {
+    this.setState({
+      checkedMenu: !this.state.checkedMenu,
+      checkedChat: this.state.checkedChat
+    })
+  }
+
+  handleChangeChat() {
+    this.setState({
+      checkedMenu: this.state.checkedMenu,
+      checkedChat: !this.state.checkedChat
+    })
+  }
+
+  render(){
+    return (
+        <div>
+          <Menu {...this.props} state={this.state}/>
+          <Chat {...this.props} state={this.state}/>
+          <Footer {...this.props} state={this.state} handleChangeMenu={this.handleChangeMenu} handleChangeChat={this.handleChangeChat}/>
+        </div>
+    );
+  }
 }
 
 export default App;
