649781b093421c80d4aec33be8523260437416d1
Daniel Matuszewski <d.matuszewski@lawbiz.pl>
2020-04-21 19:09:11 +0200

    Communictaion with Database

---
+++ database/DatabaseConnection.php
@@ -0,0 +1,72 @@
+<?php
+	require_once("DatabaseCreate.php");
+	
+	class DatabaseConnection {
+		public $conn = null;
+		
+		function connect(){
+			$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "bazaInz";
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+
+			// Check connection
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+			echo "Connected successfully<br>";
+			return $conn;
+		}
+		
+		 function get_name() {
+			return $this->name;
+		}
+	
+		
+		function insertData($conn,$table,$data,$colNames){
+				$colNamesS = join(', ', $colNames);
+				
+				for($i = 0; $i < 2 ; $i++){
+				
+				$dataS = join('"," ', $data[$i]);
+				$sql = "INSERT INTO $table ($colNamesS) VALUES (\"$dataS\")";
+				
+				if ($conn->query($sql) === TRUE) {
+					echo "Row $i added to $table successfully!<br>";
+				} else {
+					echo "Error inserting data: " . $conn->error ."<br>";
+				}
+				}
+				
+		}
+		
+		function getRow($table){
+			
+			
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+
+		$sql = "SELECT * FROM $table";
+		$result = $conn->query($sql);
+		
+		return $sql;
+		}
+		
+		
+		function closeConnection($conn){
+			$conn->close();
+		}
+	}
+	
+$db = new DatabaseConnection();
+createDatabase();
+$conn = $db->connect();
+createTable($conn);
+$colNames = ["id", "login", "pass","role" ];
+$data = [[1,"Daniel","Matuszewski","Admin"],[2,"Wojtek", "Jaskowiak","User"]];
+$db->insertData($conn,"userTable",$data,$colNames);
+$db->closeConnection($conn);
+	?>
---
+++ database/DatabaseCreate.php
@@ -0,0 +1,42 @@
+<?php
+	function createDatabase(){
+			
+			$servername = "localhost";
+			$username = "root";
+			$password = "";
+			// Create connection
+			$conn = new mysqli($servername, $username, $password);
+
+			// Check connection
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+			echo "Connected successfully\n";
+			
+			
+			// Create database
+			$sql = "CREATE DATABASE bazaInz";
+			if ($conn->query($sql) === TRUE) {
+			echo "Database created successfully";
+			} else {
+			echo "Error creating database: " . $conn->error;
+			}
+			}
+		
+		function createTable($conn){
+			
+			// sql to create table
+			$sql = "CREATE TABLE userTable (
+			id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+			login VARCHAR(30) NOT NULL,
+			pass VARCHAR(30) NOT NULL,
+			role VARCHAR(30)
+			) ";
+
+			if ($conn->query($sql) === TRUE) {
+			echo "Table MyGuests created successfully\n";
+			} else {
+				echo "Error creating table: " . $conn->error;
+			}
+		}
+		?>
