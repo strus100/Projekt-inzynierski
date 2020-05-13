050734e9db407f0b56cb476391fce476d161707c
Djradki <djradki@gmail.com>
2020-05-13 22:17:53 +0200

    LDAP authorization almost finished

--- login_system/ldap.php
+++ login_system/ldap.php
@@ -1,43 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>LDAP test</title>
-    </head>
-    <body>
-        <?php
-            if(isset($_POST['login']) && isset($_POST['password'])){
-                $address = "ldap://labs.wmi.amu.edu.pl";
-                $root = "DC=labs,DC=wmi,DC=amu,DC=edu,DC=pl";
-                $login = htmlspecialchars($_POST['login']);
-                $password = htmlspecialchars($_POST['password']);
-                $filter = "(cn=$login)";
+<?php
+    if(isset($_POST['JSON'])){
+        $decoded_JSON_array = json_decode($_POST['JSON'], true);
+        if(htmlspecialchars($decoded_JSON_array['type']) !== "login") die();
+        
+        $address = "ldap://labs.wmi.amu.edu.pl";
+        $root = "OU=People,DC=labs,DC=wmi,DC=amu,DC=edu,DC=pl";
+        $login = htmlspecialchars($decoded_JSON_array['login']);
+        $password = htmlspecialchars($decoded_JSON_array['password']);
+        $filter = "(cn=$login)";
 
-                $conn = ldap_connect($address, 636) or die("Error connecting");
-                ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
+        $conn = ldap_connect($address, 636) or die("{\"type\": \"login\", \"login\": 0, \"access\": \"connection\"}");
+        ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
 
-                $bind = ldap_bind($conn, $login."@labs.wmi.amu.edu.pl", $password);
-                if($bind){
-                    echo "<br>Logged in!<br><br>";
-                    $result = ldap_search($conn, $root, $filter);
-                    echo ldap_error($conn)."<br><br>";
-                    $data = ldap_get_entries($conn, $result);
-                    echo "<pre>";
-                    print_r($data);
-                    echo "</pre>";
-
-                }else{
-                    echo "<br>ERROR<br><br>";
-                }
-
-                ldap_close($conn);
+        $bind = ldap_bind($conn, $login."@labs.wmi.amu.edu.pl", $password);
+        if($bind){
+            $result = ldap_search($conn, $root, $filter);
+            $data = ldap_get_entries($conn, $result);
+            if($data["count"] != 1) die("{\"type\": \"login\", \"login\": 0, \"access\": \"count\"}");
+            switch (strtolower($data[0]["description"][0])) {
+                case 'student':
+                    echo "{\"type\": \"login\", \"login\": 1, \"access\": \"student\"}";
+                    break;
+                case 'doktorant':
+                    echo "{\"type\": \"login\", \"login\": 1, \"access\": \"doktorant\"}";
+                    break;
+                case 'pracownik':
+                    echo "{\"type\": \"login\", \"login\": 1, \"access\": \"pracownik\"}";
+                    break;
+                default:
+                    echo "{\"type\": \"login\", \"login\": 0, \"access\": \"login\"}";
+                    break;
             }
-        ?>
-        <form action="#" method="POST">
-            <input type="text" name="login" id="login" placeholder="LOGIN">
-            <input type="password" name="password" id="password" placeholder="PASSWORD">
-            <input type="submit" value="zaloguj">
-        </form>
-    </body>
-</html>
+            
+        }else{
+            echo "{\"type\": \"login\", \"login\": 0, \"access\": \"login\"}";
+        }
+
+        ldap_close($conn);
+    }
+?>
