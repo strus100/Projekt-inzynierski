552e61bb698ce0faefce0018d901056454c9451f
Djradki <djradki@gmail.com>
2020-06-07 21:19:37 +0200

    improving ldap

--- login_system/ldap.php
+++ login_system/ldap.php
@@ -1,12 +1,11 @@
 <?php
-    if(isset($_POST['JSON'])){
-        $decoded_JSON_array = json_decode($_POST['JSON'], true);
-        if(htmlspecialchars($decoded_JSON_array['type']) !== "login") die();
-        
+    require_once __DIR__."/../database/DatabaseConnection.php";
+
+    if( isset($_POST['login']) && isset($_POST['password']) ){
         $address = "ldap://labs.wmi.amu.edu.pl";
         $root = "OU=People,DC=labs,DC=wmi,DC=amu,DC=edu,DC=pl";
-        $login = htmlspecialchars($decoded_JSON_array['login']);
-        $password = htmlspecialchars($decoded_JSON_array['password']);
+        $login = htmlspecialchars($_POST['login']);
+        $password = htmlspecialchars($_POST['password']);
         $filter = "(cn=$login)";
 
         $conn = ldap_connect($address, 636) or die("{\"type\": \"login\", \"login\": 0, \"access\": \"connection\"}");
@@ -20,12 +19,15 @@
             switch (strtolower($data[0]["description"][0])) {
                 case 'student':
                     echo "{\"type\": \"login\", \"login\": 1, \"access\": \"student\"}";
+                    fillDatabase($login, "student");
                     break;
                 case 'doktorant':
                     echo "{\"type\": \"login\", \"login\": 1, \"access\": \"doktorant\"}";
+                    fillDatabase($login, "s");
                     break;
                 case 'pracownik':
                     echo "{\"type\": \"login\", \"login\": 1, \"access\": \"pracownik\"}";
+                    fillDatabase($login, "pracownik");
                     break;
                 default:
                     echo "{\"type\": \"login\", \"login\": 0, \"access\": \"login\"}";
@@ -35,7 +37,33 @@
         }else{
             echo "{\"type\": \"login\", \"login\": 0, \"access\": \"login\"}";
         }
-
         ldap_close($conn);
+    }
+
+    function fillDatabase($login, $role){
+        $random = rand().microtime().rand();
+        $token = md5($random);
+        
+        $db = new DatabaseConnection();
+        $conn = $db->connect();
+        $stmt = $conn->prepare("SELECT * FROM userTable WHERE login = ?");
+        $stmt->bind_param('s', $login);
+        $stmt->execute();
+        $stmt->store_result();
+
+        if($stmt->num_rows == 1){
+            $stmt->close();
+            $stmt = $conn->prepare("UPDATE userTable SET `login`=?, `role`=?, `token`=?");
+            $stmt->bind_param('sss', $login, $role, $token);
+            $stmt->execute();
+        }
+        else{
+            $stmt->close();
+            $stmt = $conn->prepare("INSERT INTO userTable VALUES (?, ?, ?)");
+            $stmt->bind_param('sss', $login, $role, $token);
+            $stmt->execute();
+        }
+
+        $conn->close();
     }
 ?>
