d4cc8d49467ca0231380071dd3c3074ab6835856
strus100 <danek130@gmail.com>
2020-06-10 14:05:05 +0200

    More stun servers

--- WEBRTC/RTCMultiConnection.js
+++ WEBRTC/RTCMultiConnection.js
@@ -1483,7 +1483,27 @@ var RTCMultiConnection = function(roomid, forceOptions) {
 
             var servers = {
                 iceServers: [{
-                    urls: 'stun:stun.l.google.com:19302'
+                    urls: 
+					'stun:stun.l.google.com:19302',
+				'stun:stun01.sipphone.com'
+'stun:stun.ekiga.net'
+'stun:stun.fwdnet.net'
+'stun:stun.ideasip.com'
+'stun:stun.iptel.org'
+'stun:stun.rixtelecom.se'
+'stun:stun.schlund.de'
+'stun:stun.l.google.com:19302'
+'stun:stun1.l.google.com:19302'
+'stun:stun2.l.google.com:19302'
+'stun:stun3.l.google.com:19302'
+'stun:stun4.l.google.com:19302'
+'stun:stunserver.org'
+'stun:stun.softjoys.com'
+'stun:stun.voiparound.com'
+'stun:stun.voipbuster.com'
+'stun:stun.voipstunt.com'
+'stun:stun.voxgratia.org'
+'stun:stun.xten.com'
                 }]
             };
 
