9f05737a245318b6fae52d0cc5a5bfb80d7b400a
Djradki <djradki@gmail.com>
2020-11-06 17:21:28 +0100

    🚧 new lines

--- SERVER/Application/MessageService.php
+++ SERVER/Application/MessageService.php
@@ -1,7 +1,7 @@
 <?php
     class MessageService{
         static function createMessage(){
-            
+
         }
     }
 ?>
--- SERVER/Domain/Message.php
+++ SERVER/Domain/Message.php
@@ -12,6 +12,6 @@
             $this->room = $room;
         }
 
-        
+
     }
 ?>
