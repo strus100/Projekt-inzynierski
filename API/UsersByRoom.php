87c13e96739ee01d073c75c492656a8db83d982b
strus100 <danek130@gmail.com>
2020-12-05 15:33:19 +0100

    Create UsersByRoom.php

---
+++ API/UsersByRoom.php
@@ -0,0 +1,36 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		require_once __DIR__."/../database/DatabaseConnection.php";
+
+        $roomId = $_GET["roomId"];
+		$DB = new DatabaseConnection();
+        $DB->connect();
+            if( $row = $DB->getRoom( htmlspecialchars($roomId) ) ){
+				$login = $row['login'];
+				
+				if( $row2 = $DB->getUserByLogin( htmlspecialchars($login) ) ){
+					 if (!isset($user)) { 
+					$user = new stdClass();
+				}
+				
+                $user->login = $row2['login'];
+                $user->name = $row2['name'];
+                $user->surname = $row2['surname'];		
+				$user->email = $row2['email'];
+				$user->token = $row2['token'];
+				$DB->closeConnection();
+				
+				echo json_encode($user);
+            }else{
+               $DB->closeConnection();
+                return false;
+            }
+				
+				
+            }else{
+               $DB->closeConnection();
+                return false;
+            }
+} else { return false; }
+
+?>
