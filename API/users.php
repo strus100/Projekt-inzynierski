fe6aa8a324c5e5475b46c448aae852fe630fa46d
strus100 <danek130@gmail.com>
2020-12-05 13:36:37 +0100

    update api
    Change from requaire token to login, becasue token can change with session change

--- API/users.php
+++ API/users.php
@@ -2,10 +2,10 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		require_once __DIR__."/../database/DatabaseConnection.php";
 
-        $userToken = $_GET["token"];
+        $userLogin = $_GET["login"];
 		$DB = new DatabaseConnection();
         $DB->connect();
-            if( $row = $DB->getUserByToken( htmlspecialchars($userToken) ) ){
+            if( $row = $DB->getUserByLogin( htmlspecialchars($userLogin) ) ){
 				if (!isset($user)) { 
 					$user = new stdClass();
 				}
@@ -14,6 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $user->name = $row['name'];
                 $user->surname = $row['surname'];		
 				$user->email = $row['email'];
+				$user->token = $row['token'];
 				$DB->closeConnection();
 				
 				echo json_encode($user);
