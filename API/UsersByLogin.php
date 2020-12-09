857ada576371e28cccee2372e70af38c255399ec
strus100 <danek130@gmail.com>
2020-12-09 09:58:20 +0100

    API v3

---
+++ API/UsersByLogin.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		require_once __DIR__."/../database/DatabaseConnection.php";
+
+        $login = $_GET["login"];
+		$DB = new DatabaseConnection();
+        $DB->connect();
+        if( $row2 = $DB->getUserByLogin( htmlspecialchars($login) ) ){
+			if (!isset($user)) { 
+				$user = new stdClass();
+			}
+				
+                $user->login = $row2['login'];
+                $user->name = $row2['name'];
+                $user->surname = $row2['surname'];		
+				$user->email = $row2['email'];
+				$user->token = $row2['token'];
+				$DB->closeConnection();
+				
+				echo json_encode($user);
+            }else{
+               $DB->closeConnection();
+                return false;
+            }
+} else { return false; }
+
+?>
