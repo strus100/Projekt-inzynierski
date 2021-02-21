10dec634c202acf4bf57966313f4f70f814e86c1
Djradki <djradki@gmail.com>
2021-02-21 12:02:00 +0100

    🐛 quick database has gone away fix

--- SERVER/Infrastructure/Database.php
+++ SERVER/Infrastructure/Database.php
@@ -23,7 +23,15 @@
         }
 
         public function query($query){
-            return $this->connection->query($query);
+            $result = $this->connection->query($query);
+
+            if(!$result){
+                $this->logger->error("Could not execute SQL query: ".$this->connection->error);
+                $res = $this->connection->connect(Config::DB_SERVER_NAME, Config::DB_USERNAME, Config::DB_PASSWORD, Config::DB_NAME);
+                return $this->connection->query($query);
+            }else{
+                return $result;
+            }
         }
     }
 ?>
